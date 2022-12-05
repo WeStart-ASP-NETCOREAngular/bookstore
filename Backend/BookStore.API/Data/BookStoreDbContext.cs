e4d17f76ff4da37583f27153dded2b673d534683
Wasim Fatair <wasim.fatair@gmail.com>
2022-12-05 08:18:03 +0200

    Api structure

---
+++ Backend/BookStore.API/Data/BookStoreDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookStore.API.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.API.Data
+{
+    public class BookStoreDbContext : IdentityDbContext<AppUser>
+    {
+        public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Backend/BookStore.API/Models/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BookStore.API.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsActive { get; set; } = true;
+
+    }
+}
