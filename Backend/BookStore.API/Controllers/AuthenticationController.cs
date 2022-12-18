683cf508b33b48add87ccbc24b6854ae0db67fb0
Wasim Fatair <wasim.fatair@gmail.com>
2022-12-18 16:24:51 +0200

    Authentication Controller ( Login & register )

---
+++ Backend/BookStore.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,80 @@
+﻿using BookStore.API.DTOs;
+using BookStore.API.Interfaces;
+using BookStore.API.Models;
+using MapsterMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.API.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IJwtTokenGenerator _jwtToken;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IMapper _mapper;
+
+        public AuthenticationController(IJwtTokenGenerator jwtToken, UserManager<AppUser> userManager, IMapper mapper)
+        {
+            _jwtToken = jwtToken;
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(LoginRequest request)
+        {
+
+            var user = await _userManager.FindByEmailAsync(request.email);
+
+            if (user != null && await _userManager.CheckPasswordAsync(user, request.password))
+            {
+                if (user.IsActive)
+                {
+                    var token = await _jwtToken.Generate(user);
+                    return Ok(token);
+                }
+                else
+                    return Unauthorized(new { message = "unactive account, please contact administrator" });
+
+            }
+            else
+                return Unauthorized(new { message = "Unauthorized" });
+        }
+
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> RegisterAsync(RegisterRequest request)
+        {
+
+            var userExists = await _userManager.FindByEmailAsync(request.Email);
+            if (userExists != null)
+            {
+                return BadRequest("User exists!");
+            }
+
+            var identityUser = _mapper.Map<AppUser>(request);
+
+            var result = await _userManager.CreateAsync(identityUser, request.Password);
+            if (!result.Succeeded)
+            {
+                var errors = result.Errors.Select(x => x.Description);
+                return BadRequest(new { Errors = errors });
+            }
+
+            await _userManager.AddToRoleAsync(identityUser, "User");
+
+            var token = await _jwtToken.Generate(identityUser);
+
+            return Ok(token);
+
+
+        }
+
+
+
+    }
+}
--- Backend/BookStore.API/Controllers/PublisherController.cs
+++ Backend/BookStore.API/Controllers/PublisherController.cs
@@ -3,42 +3,42 @@ using BookStore.API.Interfaces;
 using BookStore.API.Models;
 using BookStore.API.Services;
 using MapsterMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BookStore.API.Controllers
+namespace BookStore.API.Controllers;
+
+[Route("api/publisher")]
+[ApiController]
+public class PublisherController : ControllerBase
 {
-    [Route("api/publisher")]
-    [ApiController]
-    public class PublisherController : ControllerBase
+    private readonly IPublisherRepository _repository;
+    private readonly IImageUploaderService _uploaderService;
+    private readonly IMapper _mapper;
+
+    public PublisherController(IPublisherRepository repository,
+        IImageUploaderService uploaderService,
+        IMapper mapper)
     {
-        private readonly IPublisherRepository _repository;
-        private readonly IImageUploaderService _uploaderService;
-        private readonly IMapper _mapper;
+        _repository = repository;
+        _uploaderService = uploaderService;
+        _mapper = mapper;
+    }
 
-        public PublisherController(IPublisherRepository repository,
-            IImageUploaderService uploaderService,
-            IMapper mapper)
-        {
-            _repository = repository;
-            _uploaderService = uploaderService;
-            _mapper = mapper;
-        }
+    [HttpGet]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> Get()
+    {
+        return Ok(await _repository.GetAll());
+    }
 
-        [HttpGet]
-        public async Task<IActionResult> Get()
-        {
-            return Ok(await _repository.GetAll());
-        }
+    [HttpPost]
+    public async Task<IActionResult> Post([FromForm] CreatePublisherRequest request)
+    {
+        var publisher = _mapper.Map<Publisher>(request);
 
+        publisher.Logo = await _uploaderService.UploadImageAsync(request.Image);
 
-        [HttpPost]
-        public async Task<IActionResult> Post([FromForm] CreatePublisherRequest request)
-        {
-            var publisher = _mapper.Map<Publisher>(request);
-
-            publisher.Logo = await _uploaderService.UploadImageAsync(request.Image);
-
-            return Ok(await _repository.Add(publisher));
-        }
+        return Ok(await _repository.Add(publisher));
     }
 }
