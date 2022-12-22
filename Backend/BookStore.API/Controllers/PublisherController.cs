19743404ce5c11907be0af82f8da87e72838eef5
Wasim Fatair <wasim.fatair@gmail.com>
2022-12-22 14:31:48 +0200

    Getting user Id from token (HttpContext)

--- Backend/BookStore.API/Controllers/PublisherController.cs
+++ Backend/BookStore.API/Controllers/PublisherController.cs
@@ -4,7 +4,9 @@ using BookStore.API.Models;
 using BookStore.API.Services;
 using MapsterMapper;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace BookStore.API.Controllers;
 
@@ -15,20 +17,28 @@ public class PublisherController : ControllerBase
     private readonly IPublisherRepository _repository;
     private readonly IImageUploaderService _uploaderService;
     private readonly IMapper _mapper;
+    private readonly UserManager<AppUser> _userManager;
 
     public PublisherController(IPublisherRepository repository,
         IImageUploaderService uploaderService,
-        IMapper mapper)
+        IMapper mapper, UserManager<AppUser> userManager)
     {
         _repository = repository;
         _uploaderService = uploaderService;
         _mapper = mapper;
+        _userManager = userManager;
     }
 
     [HttpGet]
-    //[Authorize(Roles = "Admin")]
+    [Authorize]
     public async Task<IActionResult> Get([FromQuery] PaginationDTO paginationDTO)
-    {   
+    {
+        var userEmail = HttpContext.User.Claims
+            .FirstOrDefault(x=>x.Type == ClaimTypes.Name).Value;
+        var userId = await _userManager.FindByEmailAsync(userEmail);
+
+        //userId.Id;
+
         //HttpContext.Headers -> 1
         return Ok(await _repository.GetAll(paginationDTO));
     }
